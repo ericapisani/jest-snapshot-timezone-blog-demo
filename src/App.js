8ac3b67399b2d6725961eb20583427815670d6a4
Erica Pisani <pisani.erica@gmail.com>
2017-08-04 16:06:32 -0400

    Remove unnecessary stuff in the project.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import moment from 'moment';
 import './App.css';
 
 class App extends Component {
   render() {
+    const currentDay = moment().format("MMM Do YYYY");
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        Hello world!
+        <p>The current day is {currentDay}</p>
       </div>
     );
   }
