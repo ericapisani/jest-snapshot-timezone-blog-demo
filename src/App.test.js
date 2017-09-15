09759d40aaed1b5d6ab778439da7ef24b816dcab
Erica Pisani <pisani.erica@gmail.com>
2017-09-15 17:32:18 -0400

    Enforce a timezone in our test

--- src/App.test.js
+++ src/App.test.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import renderer from 'react-test-renderer';
-import moment from 'moment';
+import moment from 'moment-timezone';
 
 
 it('renders without crashing', () => {
+  moment.tz.setDefault('EST');
   let props = {
     currentDay: moment("2017-09-15 09:30:00").format("MMM Do YYYY h:mm:ss a")
   };
