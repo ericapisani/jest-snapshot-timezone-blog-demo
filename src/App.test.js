903ee1eefa99cc574d9bb6fdf524800d314fe409
Erica Pisani <pisani.erica@gmail.com>
2017-09-15 17:22:53 -0400

    Update gitlab-ci YAML file. Update snapshot test so it always succeeds locally.

--- src/App.test.js
+++ src/App.test.js
@@ -2,9 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import renderer from 'react-test-renderer';
+import moment from 'moment';
 
 
 it('renders without crashing', () => {
-  const tree = renderer.create(<App />).toJSON();
+  let props = {
+    currentDay: moment("2017-09-15 09:30:00").format("MMM Do YYYY h:mm:ss a")
+  };
+
+  const tree = renderer.create(<App {...props} />).toJSON();
   expect(tree).toMatchSnapshot();
 });
